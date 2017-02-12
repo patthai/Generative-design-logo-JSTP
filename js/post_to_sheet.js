9f5b6f105aecb2422a9ff66682e675507764620d
pat <pp.psuwit@gmail.com>
2017-02-12 00:15:54 -0700

    uploading polish!
    uploading polish!

--- js/post_to_sheet.js
+++ js/post_to_sheet.js
@@ -1,40 +1,53 @@
-
-jQuery( document ).ready(function( $ ) {
+$(document).ready(function( $ ) {
 	
-	var request;
-	$("#foo").submit(function(event){
-		if (request) {
-			request.abort();
-		}
-		var $form = $(this);
-		var $inputs = $form.find("input, select, button, textarea");
-		var serializedData = $form.serialize();
 	
-		$inputs.prop("disabled", true);
-		$('#result').text('Sending data...');
 	
-		request = $.ajax({
-			url: "https://script.google.com/macros/s/AKfycbwGWMMJDHvxGI0WVP1G5OasN2zOmfoRsfHGw1GqSnhaysvgGufs/exec",
-			type: "post",
-			data: serializedData
-		});
+	window.post_to_sheet = function(){
+	    var count = 0;
+		var request;
+		$("#data").submit(function(event){
+		
+		if (count < 1){
+			var $form = $("#data");
+			var $inputs = $form.find("input, select, button, textarea");
+			var serializedData = $form.serialize();
 	
-		request.done(function (response, textStatus, jqXHR){
-			$('#result').html('<a href="https://docs.google.com/spreadsheets/d/17jh5-1uCEFY0haO4I55i7N2nh26kcPsjeyGEF2JmoWI/edit?usp=sharing" target="_blank">Success - see Google Sheet</a>');
-			console.log("Hooray, it worked!");
+			$inputs.prop("disabled", true);
+			$('#result').text('Sending data...');
+	
+			request = $.ajax({
+				url: "https://script.google.com/macros/s/AKfycbwGWMMJDHvxGI0WVP1G5OasN2zOmfoRsfHGw1GqSnhaysvgGufs/exec",
+				type: "post",
+				data: serializedData
+			});
+	
+			request.done(function (response, textStatus, jqXHR){
+				$('#result').html('<a href="https://docs.google.com/spreadsheets/d/17jh5-1uCEFY0haO4I55i7N2nh26kcPsjeyGEF2JmoWI/edit?usp=sharing" target="_blank">Success - see Google Sheet</a>');
+				console.log("Hooray, it worked!");
 			
-		});
-		request.fail(function (jqXHR, textStatus, errorThrown){
-			console.error(
+			});
+			request.fail(function (jqXHR, textStatus, errorThrown){
+				console.error(
 				"The following error occured: "+
 				textStatus, errorThrown
 			);
-		});
+			});
 	
-		request.always(function () {
+			request.always(function () {
 			$inputs.prop("disabled", false);
+			});
+	
+			event.preventDefault();
+			}
+			count ++;
 		});
 	
-		event.preventDefault();
-	});
+	}
+	
+	
 });
+
+function post_it()
+{
+	post_to_sheet();
+}

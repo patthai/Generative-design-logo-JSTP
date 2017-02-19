887cc755496ff213c590baf6fca5ea9431cf9757
pat <pp.psuwit@gmail.com>
2017-02-18 21:51:02 -0700

    evolution is done!
    evolution is done!

--- js/load_sheet_evolution.js
+++ js/load_sheet_evolution.js
@@ -1,77 +1,57 @@
-function load_original_sequence(gallery_switch)
+function load_original_sequence()
 	{
 	
-	document.getElementById("gallery").remove();
 	var url = "https://spreadsheets.google.com/feeds/list/17jh5-1uCEFY0haO4I55i7N2nh26kcPsjeyGEF2JmoWI/od6/public/basic?alt=json";
+	var i = 0;
 	
+
+ load_original_sequence.move_scene =  function move_scene(direction){
 
  $.getJSON(url, function(data) // Get JSON from google sheet
  	{
-		
 		var JSON_data = data.feed.entry;
 		var JSON_length = JSON_data.length;
 		
 		
-		var i = 0;
-		var count_to_three = 0;
+		if ( direction == 1 && i < JSON_data.length)
+		{
+		 i++;
+		}
 		
-		$( ".container" ).append( $( "<div class=\"row\" id =\"gallery\"></div>" ) );
-				
+		else if ( direction == 0 && i > 0)
+		{
+		 i--;
+		}
 		
-		function load_gallery()
+		
+		//original sequence is the starting point for mutation;
+		//var original_sequence = JSON [];
+		//console.log (JSON_data.length);
+		
+		
+		var string_data = JSON_data[i].content.$t;
+		//<*********** convert each line to JSON ***********>
+		string_data = "{" + "\"" + string_data.replace(/:/g,"\":\"") +  "\"" + "}" ;
+		string_data = string_data.replace(/,/g,"\",\"");
+		string_data = string_data.replace(/\" /g,"\"");
+		var final_data = JSON.parse(string_data);
+		//<*********** convert each line to JSON ***********>
+		var original_sequence = final_data.originalsequence; 
+		
+		////////console.log(final_data);
+		document.getElementById("generation").innerHTML = "Generation " + i +  " by " + final_data.name;
+		if (final_data.comment != null)
 			{
-				
-				
-				var string_data = JSON_data[i].content.$t;
-					//<*********** convert each line to JSON ***********>
-				string_data = "{" + "\"" + string_data.replace(/:/g,"\":\"") +  "\"" + "}" ;
-				string_data = string_data.replace(/,/g,"\",\"");
-				string_data = string_data.replace(/\" /g,"\"");
-				var final_data = JSON.parse(string_data);
-				//<*********** convert each line to JSON ***********>
-				var original_sequence = final_data.originalsequence;
-			
-				var canvas_name = String ("myCanvas_"+i);
-				var comment_text = final_data.comment;
-				if (comment_text == null)
-					{
-					comment_text = " ";
-					}
-			
-				if (gallery_switch == 1)
-					{
-					if (final_data.comment == null ) 
-						{
-						i++;
-		   				load_gallery();
-						}	
-					}
-				
-					//$( ".container" ).append( $( "<div class=\"row\">" ) );
-					$( ".row" ).append( $( "<div class=\"col-md-4\"><canvas id=\"" + canvas_name + "\"width=\"400\" height=\"400\" ></canvas> <center style=\" margin-top: 20px;\"><center><h3> Generation " + i + "</center></h3><center><p>" + comment_text + "</center></p>" + final_data.name + "</p></center>" ) );
-					draw_init (canvas_name,original_sequence);
-			
-					
-				
-				
-				
-		   		if (i < JSON_length)
-		   			{
-		   			i++;
-		   			load_gallery();
-		   			}
+			document.getElementById("comment").innerHTML = final_data.comment;
 			}
+		else{document.getElementById("comment").innerHTML = "";}
+		draw_init ('only_canvas',original_sequence);
 		
-		
-		load_gallery();
-		
-
-		 	
+ 		 	
  	});
+ 	}
+  load_original_sequence.move_scene(100);
 
 }
  
 
-
-
-	
